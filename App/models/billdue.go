42ad20b256bcc342d9943f60d12ed7999535c583
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-06-25 00:22:08 -0500

    Cleaned up some print statements and commented

--- App/models/billdue.go
+++ App/models/billdue.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"os"
 )
 
@@ -9,33 +8,24 @@ func BillDueDates() (map[string]map[string]string, error) {
 	finalPayload := make(map[string]map[string]string)
 	xAxis := [5]string{"category", "Account", "Automated", "DueDate"}
 
-	//Category	Account	Automated	DueDate
 	spreadsheetId := os.Getenv("SHEETID")
-	// columnID := MothToColumnDashboard[month]
-	// columnIndex := ColumnToIndexDashboard[columnID]
 
-	fmt.Printf("Spreadsheet id %s \n", spreadsheetId)
 	readRange := "BillDueDates!D7:H19"
-
-	fmt.Println(readRange)
 	resp, err := SheetService.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println(len(resp.Values))
 	for index := 1; index < len(resp.Values); index++ {
-		// fmt.Println(resp.Values[index][0].(string))
 		payload := make(map[string]string)
 		var billName string
 		for rows := 1; rows < len(resp.Values[index]); rows++ {
-			fmt.Println(resp.Values[1][rows].(string))
-			payload[xAxis[rows-1]] = resp.Values[1][rows].(string)
-			billName = resp.Values[index][0].(string)
+
+			payload[xAxis[rows-1]] = resp.Values[1][rows].(string) //category as the key and the values
+			billName = resp.Values[index][0].(string)              // extrating out the name of the bill
 		}
-		finalPayload[billName] = payload
-		fmt.Println(payload)
-		fmt.Println()
+		finalPayload[billName] = payload //using the bill name as the key and the map of categories and values as the value
 	}
 
 	return finalPayload, nil
